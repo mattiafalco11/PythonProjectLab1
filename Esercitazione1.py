85015348fd9589704ce98ceeaf3db435584132a1
Mattia Falco <mattiafalco87@gmail.com>
2025-11-24 20:44:29 +0100

    Initial commit

---
+++ Esercitazione1.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+iris = load_iris()
+
+X = iris['data']
+y = iris['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+p_train = model.predict(X_train)
+p_test = model.predict(X_test)
+
+mae_train = mean_absolute_error(y_train, p_train)
+print('Mean Absolute Error train:', mae_train)
+mae_test = mean_absolute_error(y_test, p_test)
+print('Mean Absolute Error test:', mae_test)
